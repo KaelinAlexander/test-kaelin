2957bf401c3532d32c9003b4be56d524b39ad146
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-16 19:34:29 +0000

    first commit

---
+++ lib/test/kaelin.rb
@@ -0,0 +1,8 @@
+require "test/kaelin/version"
+
+module Test
+  module Kaelin
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test/kaelin/version.rb
@@ -0,0 +1,5 @@
+module Test
+  module Kaelin
+    VERSION = "0.1.0"
+  end
+end
